1e95ae7c5b3a6688e3dce559dbd86e9bddca43f5
Dušan Mitrović <dusan@dusanmitrovic.xyz>
2022-12-15 22:47:56 +0100

    Move to using one file per handler function.
    Signed-off-by: Dušan Mitrović <dusan@dusanmitrovic.xyz>

--- pkg/handlers/quiz/generate.go
+++ pkg/handlers/quiz/generate.go
@@ -1,1 +1,33 @@
 package quiz
+
+import (
+	"fmt"
+	"github.com/dusnm/quiz/pkg/dto"
+	"github.com/dusnm/quiz/pkg/services/password"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
+)
+
+func Generate(ctx *fiber.Ctx) error {
+	quiz := new(dto.Quiz)
+
+	if err := ctx.BodyParser(quiz); err != nil {
+		return err
+	}
+
+	if quiz.Password != "" {
+		p, err := password.New(quiz.Password)
+
+		if err != nil {
+			return err
+		}
+
+		//TODO: Write to DB
+		fmt.Println(string(p.Hash))
+	}
+
+	//TODO: Write to DB
+	uuid4 := utils.UUIDv4()
+
+	return ctx.Redirect("/quiz/" + uuid4)
+}
--- pkg/handlers/quiz/index.go
+++ pkg/handlers/quiz/index.go
@@ -1,1 +1,17 @@
 package quiz
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func Index(ctx *fiber.Ctx) error {
+	id := ctx.Params("id")
+
+	if id == "" {
+		return ctx.Redirect("/")
+	}
+
+	//TODO: Query the DB for a quiz with this id
+	// check if it's locked and then render either make or play
+	return ctx.Render("views/quiz/make", fiber.Map{})
+}
